7a71c31e916efbc43e4570ff03d1170b510b489a
atillaordog <atillaordog@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-09 12:20:28 +0000

    Version 0.2
    git-svn-id: https://plugins.svn.wordpress.org/admin-category-search@1131155 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/0.2/trunk/js/script.js
@@ -0,0 +1,64 @@
+jQuery(document).ready(function(){
+	adminSearch.init();
+});
+
+var adminSearch = {
+	init : function(){
+		jQuery.expr[':'].Contains = function(a, i, m) {
+		  return jQuery(a).text().toUpperCase()
+			  .indexOf(m[3].toUpperCase()) >= 0;
+		};
+		
+		var search_tab = '<li class="hide-if-no-js"><a href="javascript:void(0);" class="search-tab" tabindex="4">Search</a></li>';
+		jQuery('.category-tabs').append(search_tab);
+		
+		jQuery('.search-tab').each(function(){
+			var parent_id = jQuery(this).parents('.categorydiv').first().attr('id');
+			parent_id = parent_id.substring(9);
+			jQuery(this).attr('id', parent_id+'-search-tab');
+			jQuery(this).attr('href', '#'+parent_id+'-search');
+			
+			var search_div = '<div id="'+parent_id+'-search" style="display:none;padding-top:0.9em;" class="tabs-panel">';
+			search_div += '<input type="text" name="'+parent_id+'-search-field" id="'+parent_id+'-search-field" class="meta-box-search-field" />';
+			search_div += '<button type="button" id="'+parent_id+'-search-button" class="meta-box-search-button">Go</button>';
+			search_div += '<ul id="'+parent_id+'-search-results" class="meta-box-search-results"></ul>';
+			search_div +='</div>';
+			
+			
+			//jQuery(this).parents('.categorydiv').first().append(search_div);
+			jQuery(search_div).insertBefore(jQuery(this).parents('.categorydiv').first().find('.wp-hidden-children'));
+			
+			jQuery(this).parents('.categorydiv').first().append('<p><a href="javascript:void(0);" class="meta-box-show-all-link">Reset Search Results</a></p>');
+		});
+		
+		jQuery('.meta-box-search-button').live('click', function(e){
+			e.preventDefault();
+			
+			var s = jQuery(this).siblings('.meta-box-search-field').val();
+			if ( jQuery.trim(s) == "" )
+			{
+				jQuery(this).parents('.categorydiv').first().find('.categorychecklist li').show();
+				jQuery(this).parents('.categorydiv').first().find('ul li').first().find('a').trigger('click');
+			}
+			else
+			{
+				var result = jQuery(this).parents('.categorydiv').first().find('.categorychecklist li:Contains("'+s+'")');
+		
+				jQuery(this).parents('.categorydiv').first().find('.categorychecklist li').hide();
+				result.each(function(){
+					jQuery(this).show();
+				});
+				
+		
+				jQuery(this).parents('.categorydiv').first().find('ul li').first().find('a').trigger('click');
+			}
+		});
+		
+		jQuery('.meta-box-show-all-link').live('click', function(e){
+			e.preventDefault();
+			
+			jQuery(this).parents('.categorydiv').first().find('.categorychecklist li').show();
+			jQuery(this).parents('.categorydiv').find('.meta-box-search-field').val('');
+		});
+	}
+};
---
+++ tags/0.2/trunk/readme.txt
@@ -0,0 +1,39 @@
+=== WP Admin Category Search ===
+Contributors: atillaordog
+Donate link: http://attilaordog.wordpress.com/contribution/
+Tags: categories
+Requires at least: 3.0.1
+Tested up to: 4.1.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Stable Tag: trunk
+
+This plugin adds a new tab to the category boxes on admin add / edit pages.
+
+== Description ==
+
+This tab has a simple field in which you can enter a keyword and the list is filtered by this keyword.
+
+Simple as the sun, using easy JS.
+
+Especially useful when there are a lot of categories, like in case of a shop.
+
+It needs jQuery to run !
+
+== Installation ==
+
+To install the plugin, simply install it from the directory, or use the following steps
+
+1. Upload the folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Done
+
+== Screenshots ==
+
+1. This is how it looks. After you entered the keyword and pushed search, simply go back to the first tab to see the found results. 
+
+== Changelog ==
+
+= 0.2 =
+* Made the input field look nicer
+* Reset link now resets search field, too
